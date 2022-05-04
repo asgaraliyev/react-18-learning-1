97163caaa5ff1145d7448446df3d0c8df99803ad
asgaraliyev <asgaraliyev2001@gmail.com>
2022-05-04 11:16:32 +0400

    3 files modified, 2 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import './style.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Auth from './layouts/Auth';
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="auth" element={<Auth />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ layouts/Auth.tsx
@@ -0,0 +1,9 @@
+import Header from '../components/Header';
+import * as React from 'react';
+const Auth = () => (
+  <>
+    <Header />
+  </>
+);
+
+export default Auth;
